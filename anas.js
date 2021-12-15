91a7f3e681ede5035a7c96a95ad758c108d3b97e
Anas Al-kouraichi <anasalkouraichi@gmail.com>
2021-12-15 14:02:02 +0100

    anas update

---
+++ anas.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function anas(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+anas.propTypes = {
+
+}
+
+export default anas
+
